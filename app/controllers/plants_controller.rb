3d7f00ffed07005d4f6dd6226d2081b9f8308829
jtaylorjohnson <jtaylorjohnson@gmail.com>
2020-11-22 20:53:37 -0600

    debugs

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,40 +1,40 @@
 class PlantsController < ApplicationController
 
-  # GET: /plants
+
   get "/plants" do
     redirect_if_not_logged_in
     @plants = Plant.all
     erb :'plants/index'
   end
 
-  # GET: /plants/new
+
   get "/plants/new" do
     redirect_if_not_logged_in
     erb :"/plants/new"
   end
 
-  # POST: /plants
+
   post "/plants" do
     redirect_if_not_logged_in
     Plant.create(params)
     redirect "/plants"
   end
 
-  # GET: /plants/5
+
   get "/plants/:id" do
     redirect_if_not_logged_in
     @plant = Plant.find(params[:id])
     erb :"/plants/show"
   end
 
-  # GET: /plants/5/edit
+
   get "/plants/:id/edit" do
     redirect_if_not_logged_in
     @plant = Plant.find(params[:id])
     erb :"/plants/edit"
   end
 
-  # PATCH: /plants/5
+
   post "/plants/:id" do
     redirect_if_not_logged_in
     @plant = Plant.find(params[:id])
--- db/migrate/20201123002944_create_plants.rb
+++ db/migrate/20201123002944_create_plants.rb
@@ -1,7 +1,6 @@
 class CreatePlants < ActiveRecord::Migration
   def change
     create_table :plants do |t|
-
       t.string :name
       t.string :type
     end
